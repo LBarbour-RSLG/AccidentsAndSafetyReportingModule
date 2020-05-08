a19678193efae3c155007812b844fbd10b752d06
LBarbour-RSLG <64164817+LBarbour-RSLG@users.noreply.github.com>
2020-05-08 17:03:36 -0400

    Add files via upload
    Training presentation

---
+++ Accidents & Safety Training/precache-manifest.0d2b4208acbaa04063592994e56a9310.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abb3d6862689d860ec55f6394d844f8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b7a069e4f4ca87b51dfc",
+    "url": "./static/css/4.0c3ad388.chunk.css"
+  },
+  {
+    "revision": "010a0b5fd43344ca4f3d",
+    "url": "./static/css/main.b17e4a83.chunk.css"
+  },
+  {
+    "revision": "0f8c6aeb6f53d3323a34",
+    "url": "./static/js/0.08352ef1.chunk.js"
+  },
+  {
+    "revision": "6e68448eb024dfd70c62",
+    "url": "./static/js/1.aae8e2dd.chunk.js"
+  },
+  {
+    "revision": "b7a069e4f4ca87b51dfc",
+    "url": "./static/js/4.26c74ea5.chunk.js"
+  },
+  {
+    "revision": "f5ffe62bb4dcd814606253bfe72a23ef",
+    "url": "./static/js/4.26c74ea5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4901bc88a9aaf0b4a147",
+    "url": "./static/js/5.d48d53cd.chunk.js"
+  },
+  {
+    "revision": "fbe464a0cf4a06a1da9f",
+    "url": "./static/js/6.2bbb3661.chunk.js"
+  },
+  {
+    "revision": "f7183da6d3177f858272",
+    "url": "./static/js/7.425e2c17.chunk.js"
+  },
+  {
+    "revision": "010a0b5fd43344ca4f3d",
+    "url": "./static/js/main.4ef8d490.chunk.js"
+  },
+  {
+    "revision": "1e1d756c066893efefe3",
+    "url": "./static/js/runtime-main.349da2e6.js"
+  },
+  {
+    "revision": "85bb86d1265426023a89be8669575e3d",
+    "url": "./static/media/custom-icons.85bb86d1.woff2"
+  },
+  {
+    "revision": "ecc4ad1901befb9de28a21bf353b3394",
+    "url": "./static/media/custom-icons.ecc4ad19.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
